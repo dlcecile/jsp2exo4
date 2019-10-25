bbab9abad935ab4132e5617d539fb3676f0402f9
DL <deleucecile@outlook.fr>
2019-10-25 14:16:53 +0200

    fin exo4

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function efface(){ // remplace value indiqué dans le html
+    document.getElementById("lastname").value = "";
+    document.getElementById("firstname").value = "";
+    document.getElementById("city").value = "";
+}
